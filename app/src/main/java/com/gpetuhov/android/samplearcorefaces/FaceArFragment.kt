f1f09692409fd85d695c7280e4ac80ab8fa8d2d1
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-05 18:27:29 +0300

    Use front camera

--- app/src/main/java/com/gpetuhov/android/samplearcorefaces/FaceArFragment.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcorefaces/FaceArFragment.kt
@@ -1,6 +1,37 @@
 package com.gpetuhov.android.samplearcorefaces
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.google.ar.core.Config
+import com.google.ar.core.Session
 import com.google.ar.sceneform.ux.ArFragment
+import java.util.*
 
 class FaceArFragment : ArFragment() {
+
+    override fun getSessionConfiguration(session: Session): Config {
+        val config = Config(session)
+        config.augmentedFaceMode = Config.AugmentedFaceMode.MESH3D
+        return config
+    }
+
+    override fun getSessionFeatures(): Set<Session.Feature> {
+        return EnumSet.of<Session.Feature>(Session.Feature.FRONT_CAMERA)
+    }
+
+    /**
+     * Override to turn off planeDiscoveryController. Plane trackables are not supported with the
+     * front camera.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val frameLayout = super.onCreateView(inflater, container, savedInstanceState) as FrameLayout?
+
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+
+        return frameLayout
+    }
 }
